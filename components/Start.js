8aac7219cc5412cdba7f97329691f35a3e20da00
codestun <doerthy.k@gmail.com>
2023-11-22 19:41:22 +0100

    Refactor Start.js for improved styling and layout

--- components/Start.js
+++ components/Start.js
@@ -1,26 +1,66 @@
 // Start.js
 
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 const Start = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Hello Start!</Text>
-      <Button
-        title="Go to Chat"
+      <Text style={styles.appTitle}>Chat App</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder='Enter your name'
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.startChatButton}
         onPress={() => navigation.navigate('Chat')}
-      />
+      >
+        <Text style={styles.buttonText}>Start Chatting</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#8A95A5',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+    padding: 20,
+  },
+  appTitle: {
+    fontSize: 45,
+    fontWeight: '600',
+    color: '#FFFFFF',
+    marginBottom: 20,
+  },
+  inputContainer: {
+    width: '100%',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'rgba(117, 112, 131, 0.5)',
+    height: 50,
+    padding: 10,
+    marginTop: 5,
+    width: '100%',
+  },
+  startChatButton: {
+    backgroundColor: '#757083',
+    padding: 15,
+    borderRadius: 0,
+    alignItems: 'center',
+    width: '100%',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#FFFFFF',
+  },
 });
 
 export default Start;
