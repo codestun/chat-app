2e3750f45ff80309b36a635bc79f7f3ac1f78eb4
codestun <doerthy.k@gmail.com>
2023-12-20 13:40:56 +0100

    Add photo sharing via library and camera

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -9,63 +9,117 @@ import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 const CustomActions = ({ storage, onSend, userId }) => {
   const { showActionSheetWithOptions } = useActionSheet();
 
-  // Handles picking an image from the library
+  // Function to handle image picking from the library
   const pickImage = async () => {
-    const permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!permissions.granted) {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissions.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      });
+      if (!result.canceled && result.assets && result.assets.length > 0) {
+        const imageURI = result.assets[0].uri;
+        uploadAndSendImage(imageURI);
+      }
+    } else {
       Alert.alert("Permissions not granted", "You need to allow access to the library.");
-      return;
-    }
-
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-    });
-
-    if (!result.canceled && result.assets) {
-      uploadAndSendImage(result.assets[0].uri);
     }
   };
 
-  // Generates a unique reference for an image
-  const generateReference = (uri) => `${userId}-${new Date().getTime()}-${uri.split("/").pop()}`;
+  // Function to handle taking a photo using the camera
+  const takePhoto = async () => {
+    let cameraPermissions = await ImagePicker.requestCameraPermissionsAsync();
+    if (cameraPermissions.granted) {
+      let result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      });
+      if (!result.canceled) {
+        uploadAndSendImage(result.uri);
+      }
+    } else {
+      Alert.alert("Camera permission not granted", "You need to allow access to the camera.");
+    }
+  };
 
-  // Uploads the image to Firebase Storage and sends the URL as a message
+  // Function to generate a unique reference for each image
+  const generateReference = (uri) => {
+    const timeStamp = new Date().getTime();
+    const imageName = uri.split("/").pop();
+    return `${userId}-${timeStamp}-${imageName}`;
+  };
+
+  // Function to upload the image to Firebase Storage and send the image URL as a message
   const uploadAndSendImage = async (uri) => {
-    const storageRef = ref(storage, `images/${generateReference(uri)}`);
-    const blob = await (await fetch(uri)).blob();
+    const uniqueRefString = generateReference(uri);
+    const storageRef = ref(storage, `images/${uniqueRefString}`);
 
-    try {
-      const snapshot = await uploadBytes(storageRef, blob);
+    const response = await fetch(uri);
+    const blob = await response.blob();
+
+    uploadBytes(storageRef, blob).then(async (snapshot) => {
       const downloadURL = await getDownloadURL(snapshot.ref);
-      onSend([{ _id: Math.round(Math.random() * 1000000), createdAt: new Date(), user: { _id: userId }, image: downloadURL }]);
-    } catch (error) {
+      const imageMessage = {
+        _id: Math.round(Math.random() * 1000000),
+        createdAt: new Date(),
+        user: {
+          _id: userId,
+        },
+        image: downloadURL,
+      };
+      onSend([imageMessage]);
+    }).catch(error => {
       console.error("Error uploading image:", error);
-    }
+    });
   };
 
-  // Shows action sheet with options
+  // Display the action sheet with options
   const onActionPress = () => {
     const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
-    showActionSheetWithOptions({ options, cancelButtonIndex: options.length - 1 }, handleActionPress);
+    const cancelButtonIndex = options.length - 1;
+
+    showActionSheetWithOptions(
+      { options, cancelButtonIndex },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+          pickImage();
+        } else if (buttonIndex === 1) {
+          takePhoto();  // Call the takePhoto function
+        } // Add additional options as needed
+      }
+    );
   };
 
-  // Handles the selected action
-  const handleActionPress = (buttonIndex) => {
-    if (buttonIndex === 0) pickImage();
-    // Further actions like 'Take Picture' or 'Send Location' can be implemented here
-  };
-
+  // Render the '+' icon for custom actions
   return (
     <TouchableOpacity style={styles.container} onPress={onActionPress}>
-      <View style={styles.wrapper}><Text style={styles.iconText}>+</Text></View>
+      <View style={styles.wrapper}>
+        <Text style={styles.iconText}>+</Text>
+      </View>
     </TouchableOpacity>
   );
 };
 
+// Styles for the custom actions component
 const styles = StyleSheet.create({
-  container: { width: 26, height: 26, marginLeft: 10, marginBottom: 10 },
-  wrapper: { borderRadius: 13, borderColor: '#b2b2b2', borderWidth: 2, flex: 1, justifyContent: 'center', alignItems: 'center' },
-  iconText: { color: '#b2b2b2', fontWeight: 'bold', fontSize: 16, backgroundColor: 'transparent' },
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: '#b2b2b2',
+    borderWidth: 2,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconText: {
+    color: '#b2b2b2',
+    fontWeight: 'bold',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+  },
 });
 
 export default CustomActions;
