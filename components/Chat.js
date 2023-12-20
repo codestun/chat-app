44f38961c54e396339bcefe1d64ac7a249ff9b67
codestun <doerthy.k@gmail.com>
2023-12-20 13:16:01 +0100

    Implement image picking and uploading

--- components/Chat.js
+++ components/Chat.js
@@ -5,117 +5,56 @@ import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { collection, query, orderBy, onSnapshot, addDoc } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import CustomActions from './CustomActions'; // Import CustomActions component
+import CustomActions from './CustomActions';
 
 const Chat = ({ route, navigation, db, storage, isConnected }) => {
   const [messages, setMessages] = useState([]);
   const { name, backgroundColor, userId } = route.params;
 
-  // Function to handle sending messages to Firestore
-  const onSend = (newMessages = []) => {
-    if (newMessages.length > 0) {
-      const messageToSend = {
-        ...newMessages[0],
-        user: {
-          _id: userId,
-          name: name
-        },
-        createdAt: new Date() // Set the current date/time as createdAt
-      };
-      addDoc(collection(db, "messages"), messageToSend);
-    }
-  };
+  // Sends messages to Firestore
+  const onSend = (newMessages = []) => newMessages.forEach(msg => addDoc(collection(db, "messages"), { ...msg, user: { _id: userId, name }, createdAt: new Date(msg.createdAt) }));
 
-  // Custom Actions render function
-  const renderCustomActions = (props) => {
-    // Pass storage and onSend to CustomActions
-    return <CustomActions {...props} storage={storage} onSend={onSend} />;
-  };
+  // Renders custom action button for additional functionalities
+  const renderCustomActions = (props) => <CustomActions {...props} storage={storage} onSend={onSend} />;
 
-  let unsubMessages; // Declare outside useEffect
-
-  // Function to conditionally render the InputToolbar
-  const renderInputToolbar = (props) => {
-    // Only render InputToolbar if there is a network connection
-    if (isConnected) {
-      return <InputToolbar {...props} />;
-    } else return null;
-  };
+  // Conditionally renders the input toolbar based on network connectivity
+  const renderInputToolbar = (props) => isConnected ? <InputToolbar {...props} /> : null;
 
   useEffect(() => {
     navigation.setOptions({ title: name });
 
-    // Initial welcome and status messages
-    const initialMessages = [
-      {
-        _id: 2,
-        text: `${name} has entered the chat`,
-        createdAt: new Date(),
-        system: true,
-      },
-    ];
+    const initialMessages = [{ _id: 2, text: `${name} has entered the chat`, createdAt: new Date(), system: true }];
 
-    if (isConnected) {
-      // Unregister current onSnapshot listener to avoid multiple listeners
-      if (unsubMessages) unsubMessages();
-      unsubMessages = null;
+    const unsubscribe = isConnected ? setupFirestoreListener() : loadMessagesFromStorage();
 
+    return unsubscribe;
+
+    function setupFirestoreListener() {
       const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-      unsubMessages = onSnapshot(q, (snapshot) => {
-        let newMessages = [];
-        snapshot.forEach(doc => { // Ensure data structure aligns with GiftedChat
-          const firebaseData = doc.data();
-
-          // Converting the Firestore Timestamp to JavaScript Date object
-          const createdAt = firebaseData.createdAt ? new Date(firebaseData.createdAt.seconds * 1000) : new Date();
-
-          // Structure the message in the format expected by GiftedChat
-          newMessages.push({
-            _id: doc.id,
-            text: firebaseData.text,
-            createdAt,
-            user: firebaseData.user
-          });
+      return onSnapshot(q, (snapshot) => {
+        const newMessages = snapshot.docs.map(doc => {
+          const data = doc.data();
+          return { _id: doc.id, text: data.text, createdAt: new Date(data.createdAt.seconds * 1000), user: data.user, ...(data.image && { image: data.image }) };
         });
-
-        // Cache the new messages
         AsyncStorage.setItem('messages', JSON.stringify(newMessages));
-
         setMessages(GiftedChat.append(initialMessages, newMessages));
       });
-    } else {
-      // If not connected, load messages from AsyncStorage
-      AsyncStorage.getItem('messages').then(storedMessages => {
-        if (storedMessages) {
-          setMessages(JSON.parse(storedMessages));
-        }
-      });
     }
-    // Cleanup function
-    return () => {
-      if (unsubMessages) unsubMessages(); // Unsubscribe from Firestore updates
-    };
-  }, [isConnected, db, route.params]); // Add dependencies
+
+    async function loadMessagesFromStorage() {
+      const storedMessages = await AsyncStorage.getItem('messages');
+      if (storedMessages) setMessages(JSON.parse(storedMessages));
+    }
+  }, [isConnected, db, route.params]);
 
   return (
-    <View style={[styles.container, { backgroundColor: backgroundColor }]}>
-      <GiftedChat
-        messages={messages}
-        onSend={messages => onSend(messages)}
-        user={{ _id: userId, name: name }}
-        renderUsernameOnMessage
-        renderInputToolbar={renderInputToolbar} // Use the custom InputToolbar rendering function
-        renderActions={renderCustomActions} // Add renderActions prop to GiftedChat
-      />
-      {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
+    <View style={[styles.container, { backgroundColor }]}>
+      <GiftedChat messages={messages} onSend={onSend} user={{ _id: userId, name }} renderUsernameOnMessage renderInputToolbar={renderInputToolbar} renderActions={renderCustomActions} />
+      {Platform.OS === 'android' && <KeyboardAvoidingView behavior="height" />}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+const styles = StyleSheet.create({ container: { flex: 1 } });
 
 export default Chat;
--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,62 +1,71 @@
-import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
-import { useActionSheet } from '@expo/react-native-action-sheet';
+// CustomActions.js
 
-const CustomActions = ({ wrapperStyle, iconTextStyle }) => {
+import React from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, Alert } from 'react-native';
+import { useActionSheet } from '@expo/react-native-action-sheet';
+import * as ImagePicker from 'expo-image-picker';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+
+const CustomActions = ({ storage, onSend, userId }) => {
   const { showActionSheetWithOptions } = useActionSheet();
 
+  // Handles picking an image from the library
+  const pickImage = async () => {
+    const permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!permissions.granted) {
+      Alert.alert("Permissions not granted", "You need to allow access to the library.");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+
+    if (!result.canceled && result.assets) {
+      uploadAndSendImage(result.assets[0].uri);
+    }
+  };
+
+  // Generates a unique reference for an image
+  const generateReference = (uri) => `${userId}-${new Date().getTime()}-${uri.split("/").pop()}`;
+
+  // Uploads the image to Firebase Storage and sends the URL as a message
+  const uploadAndSendImage = async (uri) => {
+    const storageRef = ref(storage, `images/${generateReference(uri)}`);
+    const blob = await (await fetch(uri)).blob();
+
+    try {
+      const snapshot = await uploadBytes(storageRef, blob);
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      onSend([{ _id: Math.round(Math.random() * 1000000), createdAt: new Date(), user: { _id: userId }, image: downloadURL }]);
+    } catch (error) {
+      console.error("Error uploading image:", error);
+    }
+  };
+
+  // Shows action sheet with options
   const onActionPress = () => {
     const options = ['Choose From Library', 'Take Picture', 'Send Location', 'Cancel'];
-    const cancelButtonIndex = options.length - 1;
+    showActionSheetWithOptions({ options, cancelButtonIndex: options.length - 1 }, handleActionPress);
+  };
 
-    showActionSheetWithOptions(
-      { options, cancelButtonIndex },
-      async (buttonIndex) => {
-        switch (buttonIndex) {
-          case 0:
-            console.log('user wants to pick an image');
-            break;
-          case 1:
-            console.log('user wants to take a photo');
-            break;
-          case 2:
-            console.log('user wants to get their location');
-            break;
-        }
-      }
-    );
+  // Handles the selected action
+  const handleActionPress = (buttonIndex) => {
+    if (buttonIndex === 0) pickImage();
+    // Further actions like 'Take Picture' or 'Send Location' can be implemented here
   };
 
   return (
-    <TouchableOpacity style={[styles.container, wrapperStyle]} onPress={onActionPress}>
-      <View style={[styles.wrapper, wrapperStyle]}>
-        <Text style={[styles.iconText, iconTextStyle]}>+</Text>
-      </View>
+    <TouchableOpacity style={styles.container} onPress={onActionPress}>
+      <View style={styles.wrapper}><Text style={styles.iconText}>+</Text></View>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: 26,
-    height: 26,
-    marginLeft: 10,
-    marginBottom: 10,
-  },
-  wrapper: {
-    borderRadius: 13,
-    borderColor: '#b2b2b2',
-    borderWidth: 2,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  iconText: {
-    color: '#b2b2b2',
-    fontWeight: 'bold',
-    fontSize: 16,
-    backgroundColor: 'transparent',
-  },
+  container: { width: 26, height: 26, marginLeft: 10, marginBottom: 10 },
+  wrapper: { borderRadius: 13, borderColor: '#b2b2b2', borderWidth: 2, flex: 1, justifyContent: 'center', alignItems: 'center' },
+  iconText: { color: '#b2b2b2', fontWeight: 'bold', fontSize: 16, backgroundColor: 'transparent' },
 });
 
 export default CustomActions;
