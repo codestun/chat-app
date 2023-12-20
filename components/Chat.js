7c2a9c9b3a99cee36c8f0d564b24cdb28589032b
codestun <doerthy.k@gmail.com>
2023-12-20 14:25:38 +0100

    Render MapView for location messages

--- components/Chat.js
+++ components/Chat.js
@@ -6,19 +6,49 @@ import { GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import { collection, query, orderBy, onSnapshot, addDoc } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import CustomActions from './CustomActions';
+import MapView from 'react-native-maps';
 
 const Chat = ({ route, navigation, db, storage, isConnected }) => {
   const [messages, setMessages] = useState([]);
   const { name, backgroundColor, userId } = route.params;
 
   // Sends messages to Firestore
-  const onSend = (newMessages = []) => newMessages.forEach(msg => addDoc(collection(db, "messages"), { ...msg, user: { _id: userId, name }, createdAt: new Date(msg.createdAt) }));
+  const onSend = (newMessages = []) => {
+    newMessages.forEach(msg => {
+      addDoc(collection(db, "messages"), { ...msg, user: { _id: userId, name }, createdAt: new Date(msg.createdAt) })
+    });
+  };
 
   // Renders custom action button for additional functionalities
-  const renderCustomActions = (props) => <CustomActions {...props} storage={storage} onSend={onSend} />;
+  const renderCustomActions = (props) => {
+    return <CustomActions {...props} storage={storage} onSend={onSend} />;
+  };
 
   // Conditionally renders the input toolbar based on network connectivity
-  const renderInputToolbar = (props) => isConnected ? <InputToolbar {...props} /> : null;
+  const renderInputToolbar = (props) => {
+    return isConnected ? <InputToolbar {...props} /> : null;
+  };
+
+  // Function to render custom view for location messages
+  const renderCustomView = (props) => {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          scrollEnabled={false}
+          zoomEnabled={false}
+        />
+      );
+    }
+    return null;
+  };
 
   useEffect(() => {
     navigation.setOptions({ title: name });
@@ -34,7 +64,14 @@ const Chat = ({ route, navigation, db, storage, isConnected }) => {
       return onSnapshot(q, (snapshot) => {
         const newMessages = snapshot.docs.map(doc => {
           const data = doc.data();
-          return { _id: doc.id, text: data.text, createdAt: new Date(data.createdAt.seconds * 1000), user: data.user, ...(data.image && { image: data.image }) };
+          const message = { _id: doc.id, text: data.text, createdAt: new Date(data.createdAt.seconds * 1000), user: data.user };
+          if (data.image) {
+            message.image = data.image;
+          }
+          if (data.location) {
+            message.location = data.location;
+          }
+          return message;
         });
         AsyncStorage.setItem('messages', JSON.stringify(newMessages));
         setMessages(GiftedChat.append(initialMessages, newMessages));
@@ -49,7 +86,15 @@ const Chat = ({ route, navigation, db, storage, isConnected }) => {
 
   return (
     <View style={[styles.container, { backgroundColor }]}>
-      <GiftedChat messages={messages} onSend={onSend} user={{ _id: userId, name }} renderUsernameOnMessage renderInputToolbar={renderInputToolbar} renderActions={renderCustomActions} />
+      <GiftedChat
+        messages={messages}
+        onSend={onSend}
+        user={{ _id: userId, name }}
+        renderUsernameOnMessage
+        renderInputToolbar={renderInputToolbar}
+        renderActions={renderCustomActions}
+        renderCustomView={renderCustomView}
+      />
       {Platform.OS === 'android' && <KeyboardAvoidingView behavior="height" />}
     </View>
   );
